de42d3738c77d1d84863dd585603aac21c1b1ae5
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2021-02-16 11:59:34 +0900

    firstDrawメソッドを作成

--- classes/player.php
+++ classes/player.php
@@ -2,5 +2,15 @@
 
 class Player
 {
-  
+  private $tip;
+
+  public function __construct($tip)
+  {
+    $this->tip = $tip;
+  }
+
+  public function firstDrawPlayer($deck)
+  {
+    $hand = array_splice($deck, 0, 51);
+  }
 }
--- main.php
+++ main.php
@@ -4,3 +4,17 @@ require_once "./deck.php";
 require_once "./classes/dealer.php";
 require_once "./classes/message.php";
 require_once "./classes/player.php";
+
+$dealer = new Dealer;
+$player = new Player(10000);
+
+echo "これからブラックジャックを始めます。\n";
+echo "あなたは10000チップからスタートです\n";
+
+$dealer->firstDrawDealer($deck);
+$player->firstDrawDealer($deck);
+
+
+
+
+
